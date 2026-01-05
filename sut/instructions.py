34979f6a52ce0c8eb274990e92c6991fd1ef088c
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-01-05 16:53:25 +0300

    Clean instructions

--- sut/instructions.py
+++ sut/instructions.py
@@ -3,11 +3,14 @@ from sut.enums import OpCode
 
 
 class Instruction:
-    def __init__(self, op, arg1=-1, arg2=-1, result=-1):
-        self.op = op
-        self.arg1 = arg1
-        self.arg2 = arg2
-        self.result = result
+    """
+    Команда
+    """
+    def __init__(self, op, arg1=None, arg2=None, result=None):
+        self.op = op  # код операции
+        self.arg1 = arg1  # аргумент 1
+        self.arg2 = arg2  # аргумент 2
+        self.result = result  # результат
 
     def __repr__(self):
         return f"({self.op}, {str(self.arg1)}, {str(self.arg2)}, {self.result})"
@@ -15,10 +18,10 @@ class Instruction:
 
 class IntructionTable:
     def __init__(self):
-        self.instructions:List[Instruction] = []
-        self.orders:Dict[Instruction, int]  = {}
+        self.instructions:List[Instruction] = []  # список команд
+        self.orders:Dict[Instruction, int]  = {}  # порядковый номер команды (на всякий случай)
 
-        self.label_name = 0
+        self.label_name = 0  # следующий уникальный номер метки
 
     def __str__(self):
         text = ''
@@ -26,32 +29,37 @@ class IntructionTable:
             text += str(entry) + '\n'
         return text
 
-    def generate_instruction(self, op:OpCode, arg1=-1, arg2=-1, result=-1) -> Instruction:
+    def generate_instruction(self, op:OpCode, arg1=None, arg2=None, result=None) -> Instruction:
+        """
+        Генерация команды
+        """
         if op == OpCode.label:
-            instruction = Instruction(OpCode.label, arg1, arg2, f'L{self.label_name}')
+            instruction = Instruction(OpCode.label, arg1, arg2, f'L{self.label_name}')  # генерация метки
             self.label_name += 1
         else:
-            instruction = Instruction(op, arg1, arg2, result)
+            instruction = Instruction(op, arg1, arg2, result)  # обычная команда
         self.orders[instruction] = len(self.instructions)
         self.instructions.append(instruction)
         return instruction
 
     def make_list(self, operation: Instruction):
+        """
+        Список переходов для условных команд (backpatching)
+        """
         commands_index = [self.orders[operation]]
         return commands_index
 
     def backpatch(self, falselist: List[int], label:str):
+        """
+        Корректировка меток в переходах (backpatching)
+        """
         for index in falselist:
             self.instructions[index].result = label
 
-    def calc_address(self, record, ident):
-        field_offset = 0
-        for field in record.type.fields:
-            if field.lexem == ident.lexem:
-                field_offset = field.offset
-        return (ident, record.address + field_offset)
-
     def form_instructions_info(self):
+        """
+        Генерация текстового представления команд (для отображения на экране)
+        """
         from scanner.data import operation_codes
         text = ""
 
